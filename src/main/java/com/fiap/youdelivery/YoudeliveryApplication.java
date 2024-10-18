2e85c68797f03afda55bcc3df31b133a0f52ce98
Ed Carlos <edcarlos.adm1@gmail.com>
2024-10-17 22:51:12 -0300

    Project created

---
+++ src/main/java/com/fiap/youdelivery/YoudeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.youdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoudeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoudeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/youdelivery/YoudeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.youdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoudeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
